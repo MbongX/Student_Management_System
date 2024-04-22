95dd2eee457c0b0f36461628d5a5b59129922879
Sergiu <pavelsergiu94@yahoo.com>
2024-04-23 00:40:50 +0300

    testing

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ import User.User;
 public class Main {
     public static void main(String[] args) {
         User user = new User();
+        //the above code is used for testing at the moment
     }
 }
