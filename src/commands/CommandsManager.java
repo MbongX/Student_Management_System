c003f55b58b31d262d6e7a88834531c2c7101f1b
Guillaume <breyerguillaume@gmail.com>
2024-04-23 19:20:01 +0200

    Adding commands

--- src/commands/CommandsManager.java
+++ src/commands/CommandsManager.java
@@ -6,6 +6,14 @@ import java.util.List;
 public class CommandsManager {
     static List<Command> commandList = new ArrayList<>();
     // Below instantiation of commands
+    // @todo adding all commands
+    private final Quit quitProgram = new Quit();
     private final AddCourse addCourse = new AddCourse();
 
+    private void registerCommand (Command command) { commandList.add(command);}
+    public List<Command> getCommands() {
+        registerCommand(quitProgram);
+        registerCommand(addCourse);
+        return commandList;
+    }
 }
